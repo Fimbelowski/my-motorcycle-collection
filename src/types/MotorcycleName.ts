9409c4452631010b70ad872d839150b04163d6e8
Christopher <ChrFimbel@gmail.com>
2023-03-15 18:07:55 -0600

    Create MotorcycleName enum.

---
+++ src/types/MotorcycleName.ts
@@ -0,0 +1,8 @@
+enum MotorcycleName {
+  BmwF750GS = 'bmwF750Gs',
+  DucatiMonster = 'ducatiMonster',
+  DucatiPanigaleV4 = 'ducatiPanigaleV4',
+  indianScoutBobber = 'indianScoutBobber',
+}
+
+export default MotorcycleName;
