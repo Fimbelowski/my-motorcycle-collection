2359bcc7c6893b5e2c97188699aba29f7d7d1e41
Christopher <ChrFimbel@gmail.com>
2023-03-16 14:26:47 -0600

    Update Motorcycle.ts

--- src/types/Motorcycle.ts
+++ src/types/Motorcycle.ts
@@ -2,6 +2,7 @@ export default interface Motorcycle {
   color: string;
   description: string;
   displacement: number;
+  imgSrc: string;
   manufacturer: string;
   model: string;
   power: string;
