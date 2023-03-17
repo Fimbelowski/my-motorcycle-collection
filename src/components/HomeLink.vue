e7d2ab58e8a30109c66477a6a561ea1f79964446
Christopher <Fimbelowski@users.noreply.github.com>
2023-03-17 13:31:31 -0600

    Add functionality to NotFound page. (#6)

---
+++ src/components/HomeLink.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { RouterLink } from 'vue-router';
+
+import RouteName from '@/types/RouteName';
+</script>
+
+<template>
+  <RouterLink class="home-link" :to="{ name: RouteName.Home }">
+    <FontAwesomeIcon class="home-link__icon" icon="fa-solid fa-arrow-left" />
+    Home</RouterLink
+  >
+</template>
+
+<style lang="scss">
+.home-link {
+  display: flex;
+  align-items: center;
+
+  text-decoration: none;
+  font-size: 2.25rem;
+
+  &:link,
+  &:visited {
+    color: var(--color-blue);
+  }
+
+  &__icon {
+    margin-right: 0.75rem;
+  }
+}
+</style>
