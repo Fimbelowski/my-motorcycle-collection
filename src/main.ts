59285e1927dd527c03b89b7f6d71586180f70593
Christopher <ChrFimbel@gmail.com>
2023-03-17 13:51:48 -0600

    Use useHead.

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import { createApp } from 'vue';
+import { createHead } from '@vueuse/head';
 import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
 import { library } from '@fortawesome/fontawesome-svg-core';
 
@@ -9,11 +10,13 @@ import routes from './router/routes';
 library.add(faArrowLeft);
 
 const app = createApp(App);
+const head = createHead();
 
 const router = createRouter({
   history: createWebHistory(),
   routes,
 });
 
+app.use(head);
 app.use(router);
 app.mount('#app');
